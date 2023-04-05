5784db9f22cfd5e3307fdfb93f5cfef0608c4d56
Jesus Santiago <jesusantiago420@gmail.com>
2023-04-04 22:05:29 -0300

    se termino el diseño del portafolio

---
+++ script.js
@@ -0,0 +1,16 @@
+let list = document.querySelectorAll(".nav li");
+function active(){
+        list.forEach((i) =>
+        i.classList.remove("active"))
+    this.classList.add("active")
+}
+list.forEach((i) =>
+    i.addEventListener(("click"), active))
+
+let burger = document.querySelector(".menuToggle");
+let header = document.querySelector(".header");
+
+burger.onclick = function(){
+    header.classList.toggle("active")
+    
+}
